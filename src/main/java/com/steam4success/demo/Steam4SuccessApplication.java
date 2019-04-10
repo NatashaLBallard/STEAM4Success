f6541fd2c29de4724333e1e31f1ca5f4e6c47f12
nalaballard <nalaballard@gmail.com>
2019-04-10 19:38:26 -0400

    Initial commit

---
+++ src/main/java/com/steam4success/demo/Steam4SuccessApplication.java
@@ -0,0 +1,13 @@
+package com.steam4success.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Steam4SuccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Steam4SuccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steam4success/demo/Steam4SuccessApplicationTests.java
@@ -0,0 +1,16 @@
+package com.steam4success.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Steam4SuccessApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
